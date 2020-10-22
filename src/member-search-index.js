a76d705ec7a6ce4c13487a724f920e15eeea51c8
Amir Fazlollahi <amir.h.fazlollahi@gmail.com>
2020-10-22 18:07:58 +0330

    Creating System Voting

---
+++ src/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"Person","l":"getFirstName()"},{"p":"<Unnamed>","c":"Person","l":"getLastName()"},{"p":"<Unnamed>","c":"Person","l":"Person(String, String)","u":"%3Cinit%3E(java.lang.String,java.lang.String)"},{"p":"<Unnamed>","c":"Person","l":"toString()"}];updateSearchResults();
